8c7fa3bc017550cfb9430a080a56d2df8d480ade
laboratorio <laboratorio@UPOAULA10702>
2025-05-29 07:23:43 -0500

    Agregar archivos de proyecto.

---
+++ EjercicioClaseMAUIPages/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EjercicioClaseMAUIPages
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
